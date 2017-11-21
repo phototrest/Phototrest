2c783387cb84c53cf935439affa3d9fac6589dc1
tamier <m2ta@uwaterloo.ca>
2017-11-20 20:47:41 -0500

    Deploy to beta if build pass

--- .travis-build.sh
+++ .travis-build.sh
@@ -11,5 +11,10 @@ else
     (cd $ROOT/ && git clone --depth 1 https://github.com/phototrest/Phototrest.git )
 fi
 
+echo "------ Start Building ------"
 # Build 
 (cd $ROOT/Phototrest && mvn clean package)
+
+echo "------ Start Deploying ------"
+# Deploy to Beta
+(HEROKU_API_KEY="a486fa4c-7347-4165-a851-e1db25bf3a57" mvn heroku:deploy-war)
