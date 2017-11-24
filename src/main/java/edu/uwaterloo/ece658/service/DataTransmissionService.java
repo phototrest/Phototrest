807cd1c2d5bb1e34b88984a3624c51ad9822e290
Daniel <dgcaccam@lakeheadu.ca>
2017-11-24 11:25:58 -0500

    DataTransmissionService:
    - give front end upload and download URL
    - update database with new photo and tag information
    	**TODO: need user information to assign photo uploader**
    	**TODO: need upload time and md5**
    - delete photo from database and s3
    	**TODO: May not remove all references to photo in databse**
    
    Subscriptionservice:
    
    - notify user function called from dataTransmission
    	**TODO: send notification to user**

--- src/main/java/edu/uwaterloo/ece658/service/DataTransmissionService.java
+++ src/main/java/edu/uwaterloo/ece658/service/DataTransmissionService.java
@@ -5,7 +5,16 @@
  */
 package edu.uwaterloo.ece658.service;
 
-//import java.util.List;
+import edu.uwaterloo.ece658.util.S3Util;
+import com.amazonaws.HttpMethod;
+import edu.uwaterloo.ece658.entity.Photo;
+import edu.uwaterloo.ece658.entity.Tag;
+import edu.uwaterloo.ece658.session.PhotoFacade;
+import edu.uwaterloo.ece658.session.TagFacade;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
 /**
@@ -15,19 +24,87 @@ import javax.ejb.Stateless;
 @Stateless
 public class DataTransmissionService {
 
-//    public String upload(String pathToImage) {
-//        return null;
-//    }
-//
-//    public boolean updateS3key(String S3key, List<String> tags) {
-//        return false;
-//    }
-//
-//    public String download(String S3key) {
-//        return null;
-//    }
-//
-//    public boolean deleteImage(String S3key) {
-//        return false;
-//    }
+    @EJB
+    private SubscriptionService subscriptionService;
+
+    @EJB
+    private PhotoFacade photoFacade;
+    
+    @EJB
+    private TagFacade tagFacade;
+
+    @EJB
+    private S3Util s3Util;
+
+    public URL upload(String s3Key) {
+        URL uploadURL = s3Util.getS3Url(s3Key, HttpMethod.PUT);
+        return uploadURL;
+    }
+    
+    public URL download(String S3key) {
+        URL downloadURL = s3Util.getS3Url(S3key, HttpMethod.GET);
+        return downloadURL;
+    }
+    
+    private Photo initializePhoto(String S3key, Integer size, boolean isPrivate, List<Tag> tags) {
+        Photo photo = new Photo();
+        photo.setIsPrivate(isPrivate);
+        //photo.setMd5();
+        photo.setS3Key(S3key);
+        photo.setSize(size);
+        photo.setTags(tags);
+        //photo.setUploadedTime(uploadedTime);
+        //photo.setUploadedUsers(uploadedUsers);
+        
+        for (Tag tag : tags) {
+            tag.addPhoto(photo);
+        }
+        return photo;
+    }
+    
+    private Tag initializeTag(String tagName) {
+        Tag tag = new Tag();
+        tag.setName(tagName);
+        return tag;
+    }
+    
+    private List<Tag> initializeTags(List<String> tagNames) {
+        List<Tag> tags = new ArrayList<>();
+        
+        for (String tagName : tagNames) {
+            tags.add(initializeTag(tagName));
+        }
+        return tags;
+    }
+    
+    public boolean updateDatabase(String S3key, Integer size, boolean isPrivate, List<String> tagNames) {
+        // store key in table with tags
+        // TODO: get user
+        List<Tag> tags = initializeTags(tagNames);
+        Photo photo = initializePhoto(S3key, size, isPrivate, tags);
+        
+        photoFacade.create(photo);
+        for (Tag tag : tags) {
+            tagFacade.create(tag);
+        }
+        
+        // call notify to send messages to users
+        subscriptionService.notifyUsers(tags, photo);
+        
+        return true;
+    }
+    
+    public boolean deleteImage(String S3key) {
+        //delete key in table
+        Photo photo = photoFacade.find(S3key);
+        if (photo != null) {
+            photoFacade.remove(photo);
+            
+        } else {
+            // cannot find photo
+            return false;
+        }
+        // delete image in s3
+        return s3Util.deleteImage(S3key);
+    }
 }
--- src/main/java/edu/uwaterloo/ece658/service/SubscriptionService.java
+++ src/main/java/edu/uwaterloo/ece658/service/SubscriptionService.java
@@ -6,9 +6,12 @@
 package edu.uwaterloo.ece658.service;
 
 import javax.ejb.Stateless;
-//import com.amazonaws.services.s3.AmazonS3;
-//import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-//import java.util.List;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import edu.uwaterloo.ece658.entity.Photo;
+import edu.uwaterloo.ece658.entity.Tag;
+import edu.uwaterloo.ece658.entity.User;
+import java.util.List;
 
 /**
  *
@@ -17,23 +20,44 @@ import javax.ejb.Stateless;
 @Stateless
 public class SubscriptionService {
 
-//    final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
-//    // Add business logic below. (Right-click in editor and choose
-//    // "Insert Code > Add Business Method")
-//
-//    public boolean createNewTag(String tag) {
-//        return false;
-//    }
-//    public boolean subscribeToTag(String tag) {
-//        return false;
-//    }
-//
-//    public boolean unsubscribeFromTag(String tag) {
-//        return false;
-//    }
-//
-//    public String notifyUsers(List<String> tags, String imgURL) {
-//
-//        return null;
-//    }
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+ 
+    /*public boolean createNewTag(String tag) {
+        // creates a new tag entry into the data base and new entitiy class?
+        return false;
+    }
+    public boolean subscribeToTag(String tag) {
+        // check if tag exists
+        // if not then createNewTag(tag);
+        
+        // add tag to user's subscriptions in database
+        return false;
+    }
+
+    public boolean unsubscribeFromTag(String tag) {
+        // remove tag from user's subscriptions in database
+        return false;
+    }*/
+    
+    private void sendNotification(User user, Photo photo) {
+        // TODO: send notification? 
+    }
+    
+    private void notify(List<User> users, Photo photo) {
+        for (User user : users) {
+            sendNotification(user, photo);
+        }
+    }
+    
+    public String notifyUsers(List<Tag> tags, Photo photo) {
+        // iterate through list of users in each tag and send notification
+        for (Tag tag : tags) {
+            notify(tag.getSuscribedUsers(), photo);
+        }
+        return null;
+    }
+    
+    
+    
 }
