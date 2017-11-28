1b2fbb2c5ed7d77769f78abca4b9a93db1e716de
tamier <m2ta@uwaterloo.ca>
2017-11-28 17:07:58 -0500

    Change build target

--- .travis-build.sh
+++ .travis-build.sh
@@ -3,14 +3,6 @@ ROOT=$TRAVIS_BUILD_DIR/
 # Fail the whole script if any command fails
 set -e
 
-echo "------ Downloading Phototrest ------"
-# Clone Phototrest
-if [ -d $ROOT/Phototrest ] ; then
-    (cd $ROOT/Phototrest && git pull)
-else
-    (cd $ROOT/ && git clone --depth 1 https://github.com/phototrest/Phototrest.git )
-fi
-
 echo "------ Start Building ------"
 # Build
-(cd $ROOT/Phototrest && mvn clean package)
+(mvn test)
